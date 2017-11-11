942ea8afb347d6a2dc7acef8bafaa90e772c21d9
Kirsten Fournier <kfournier@hopkinton.k12.ma.us>
2017-11-11 14:56:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function theBeatlesPlay(musicians, instruments){
     }
     return newArray;
 }
+
+  function johnLennonFacts(facts) {
+   var i = 0;   //creates counter variable
+   var newFacts = [];   //creates new array of facts (for after the !!! is added?)
+ 
+   while (facts.length > i) {   //while facts length is greater than 0, or while a fact already exists
+     newFacts.push(facts[i] + "!!!");   //going to push !!! to the end of each fact, creating a new array
+     i++;   //going to push that !!! to the end of each fact, the i iterates over the loop of facts
+   }
+   return newFacts;   //returns the new array
+ }
+}
