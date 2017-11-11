91faf5e49502552647fefe8196706b3d6a6e63f6
Kirsten Fournier <kfournier@hopkinton.k12.ma.us>
2017-11-11 14:43:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(array.musicians, array.instruments){
+  var musicians = [];
+
+  for(var i=0;i<musicians.length;i++){
+        array.push(array.musicians);
+    }
+    return arr;
+}
